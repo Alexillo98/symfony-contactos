a1a7b3db75f93337e241bcaa9991eafb85de2253
Alexillo98 <musateduardalexandru@gmail.com>
2024-09-26 09:21:52 +0200

    base de datos de contacto, /page, /, /contacto/{codigo},/contacto/buscar/{texto}

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Contacto;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
+
+
     private $contactos = [
         1 => ["nombre" => "Juan Pérez", "telefono" => "524142432", "email" => "juanp@ieselcaminas.org"],
         2 => ["nombre" => "Ana López", "telefono" => "58958448", "email" => "anita@ieselcaminas.org"],
@@ -15,6 +19,28 @@ class PageController extends AbstractController
         7 => ["nombre" => "Laura Martínez", "telefono" => "42898966", "email" => "lm2000@ieselcaminas.org"],
         9 => ["nombre" => "Nora Jover", "telefono" => "54565859", "email" => "norajover@ieselcaminas.org"]
     ];
+
+    #[Route('/contacto/insertar', name: 'insertar_contacto')]
+    public function insertar(ManagerRegistry $doctrine)
+    {
+        $entityManager = $doctrine->getManager();
+
+        foreach ($this->contactos as $c){
+            $contacto = new Contacto();
+            $contacto->setNombre($c["nombre"]);
+            $contacto->setTelefono($c["telefono"]);
+            $contacto->setEmail($c["email"]);
+            $entityManager->persist($contacto);
+        }
+
+        try {
+            $entityManager->flush();
+            return new Response("Contactos insertados");
+        }catch (\Exception $e){
+            return new Response("Error insertando objetos");
+        }
+    }
+
     #[Route('/page', name: 'app_page')]
     public function index(): Response
     {
@@ -34,42 +60,26 @@ class PageController extends AbstractController
     #[Route('/contacto/{codigo}', name: 'ficha_contacto')] /* { } <-- significa opcional*/
     public function ficha(int $codigo): Response
     {
-        $resultado = ($this->contactos[$codigo] ?? null);
+        $contacto = ($this->contactos[$codigo] ?? null);
 
-        if ($resultado) {
-            $html = "<ul>";
-                $html .= "<li>" . $codigo . "</li>";
-                $html .= "<li>" . $resultado["nombre"] . "</li>";
-                $html .= "<li>" . $resultado["telefono"] . "</li>";
-                $html .= "<li>" . $resultado["email"] . "</li>";
-            $html .= "</ul>";
-            return new Response("<html><body>$html</body>");
+        if($contacto){
+            return $this->render('ficha_contacto.html.twig',["contacto" => $contacto]);
         }
+
         return new Response("<html><body>Contacto $codigo no encontrado</body>");
     }
 
     /*buscar*/
 
-    #[Route('/contacto/buscar/{texto}', name: 'buscar')]
-    public function buscar($texto): Response
+    #[Route('/contacto/buscar/{texto}', name: 'buscar_contacto')]
+    public function buscar(ManagerRegistry $doctrine,$texto): Response
     {
         //Filtrar aquello que tengan el texto en el nombre
         $resultados = array_filter($this->contactos, function ($contacto) use ($texto) {
-            return strpos($contacto["nombre"], $texto) !== false;
+            return strpos($contacto["nombre"], $texto) !== FALSE;
         }
         );
 
-        if(count($resultados)) {
-            $html = "<ul>";
-            foreach ($resultados as $id => $contacto) {
-                $html .= "<li>" . $id . "</li>";
-                $html .= "<li>" . $contacto["nombre"] . "</li>";
-                $html .= "<li>" . $contacto["telefono"] . "</li>";
-                $html .= "<li>" . $contacto["email"] . "</li>";
-            }
-            $html .= "</ul>";
-            return new Response("<html><body>$html</body>");
-        }
-        return new Response("<html><body>No se ha encontrado ningun contacto</body>");
+        return $this->render('lista_contactos.html.twig',['contactos'=>$resultados]);
     }
 }
